ee6bf25f0e94ea7808bdfddded95c1e937237248
yoonsubKim <camera702@naver.com>
2014-10-25 05:11:11 +0900

    $http를 이용한 통신 post 방식 테스트

--- pages/login/loginCtrl.js
+++ pages/login/loginCtrl.js
@@ -1,8 +1,10 @@
 /**
  * Created by yoonsKim on 14. 10. 25..
  */
-function loginCtrl($scope) {
+function loginCtrl($scope,$http) {
     $scope.modeLogin = 'join';
+    
+    //로그인과 회원가입의 폼을 바꾸며 보여준다
     $scope.changeLoginMode = function(event) {
         if ($(event.target).hasClass('btn-success')) {
             // 현재 모드를 각인시켜서 폼을 변경시킨다
@@ -15,6 +17,21 @@ function loginCtrl($scope) {
                 .addClass('bg-white');
         }
 
+    }
+    
+    $scope.submitJoin = function () {
+
+        $http.post('http://14.63.171.66:8081/tleafstructure/user/signup',{
+                 email1:$scope.email1
+                ,nickname:$scope.nickname
+                ,pw:$scope.pw
+                ,age:$scope.age
+                }).
+            success(function(data) {
+                // this callback will be called asynchronously
+                // when the response is available
+                console.log('data..'+data);
+            });
     }
 
 }
