05ce8659241f4998f63e63b37c5e2285e5112880
yoonsubKim <camera702@naver.com>
2014-10-25 08:39:22 +0900

    $http+ form post방식의 회원가입 통신성공

--- pages/login/loginCtrl.js
+++ pages/login/loginCtrl.js
@@ -1,9 +1,12 @@
 /**
  * Created by yoonsKim on 14. 10. 25..
  */
+
+
+
 function loginCtrl($scope,$http) {
     $scope.modeLogin = 'join';
-    
+
     //로그인과 회원가입의 폼을 바꾸며 보여준다
     $scope.changeLoginMode = function(event) {
         if ($(event.target).hasClass('btn-success')) {
@@ -18,20 +21,52 @@ function loginCtrl($scope,$http) {
         }
 
     }
-    
-    $scope.submitJoin = function () {
 
-        $http.post('http://14.63.171.66:8081/tleafstructure/user/signup',{
-                 email1:$scope.email1
-                ,nickname:$scope.nickname
-                ,pw:$scope.pw
-                ,age:$scope.age
-                }).
-            success(function(data) {
-                // this callback will be called asynchronously
-                // when the response is available
-                console.log('data..'+data);
-            });
+
+    $scope.isSamePw = true;
+    $scope.submitJoin = function (info) {
+    console.log(info);
+//        var data = {
+//            email1:'email'
+//            ,nickname:'nick'
+//            ,pw:'pw'
+//            ,age:'123'
+//        };
+        $http({method: 'POST',
+              url: 'http://14.63.171.66:8081/tleafstructure-0600/user/signup',
+              headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                 data:
+                     'email1='+info.email1+
+                     'email2=naver.com'+
+                     '&nickname='+info.nickname +
+                     '&pw='+info.pw +
+                     '&age='+info.age
+
+        }).then(function(result){
+            console.log(result);
+        }) ;
+
+
+//        $http('http://14.63.171.66:8081/tleafstructure-0600/user/signup',{
+//                 email1:'email'
+//                ,nickname:'nick'
+//                ,pw:'pw'
+//                ,age:'123'
+//                }).
+//            success(function(data) {
+//                // this callback will be called asynchronously
+//                // when the response is available
+//                console.log(data);
+//            });
     }
+//    $scope.samePw = function () {
+//        console.log($scope.pw+', '+$scope.pw2);
+//        if($scope.pw == $scope.pw2){
+//            $scope.isSamePw= true;
+//        }else{
+//        $scope.isSamePw=  false;
+//        }
+//    }
+
 
 }
